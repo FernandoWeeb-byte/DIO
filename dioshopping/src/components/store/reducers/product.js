c8fe5efbb71059998fac51348b8788f3e349b2c4
FernandoWeeb-byte <fernandobr7186@gmail.com>
2022-02-13 16:36:14 -0300

    games shopping

---
+++ dioshopping/src/components/store/reducers/product.js
@@ -0,0 +1,136 @@
+const productsList = [
+    {
+      id_product: 1,
+      category: "Ps5",
+      fk_idcategorys: 1,
+      name_product: "Final Fantasy VII",
+      price: 300,
+      image: "./images/produtos/final-fantasy-vii.jpg",
+      id_categorys: 1,
+      name_categorys: "Ps5",
+      selected: false
+    },
+    {
+      id_product: 2,
+      category: "Ps4",
+      fk_idcategorys: 2,
+      name_product: "God of War",
+      price: 350,
+      image: "./images/produtos/God-of-War---Playstation-4.jpg",
+      id_categorys: 2,
+      name_categorys: "Ps4",
+      selected: false
+    },
+    {
+      id_product: 9,
+      category: "Ps4",
+      fk_idcategorys: 2,
+      name_product: "GTA 5",
+      price: 230,
+      image: "./images/produtos/gta5.jpg",
+      id_categorys: 2,
+      name_categorys: "Ps4",
+      selected: false
+    },
+    {
+      id_product: 4,
+      category: "Xbox",
+      fk_idcategorys: 3,
+      name_product: "Halo Infinite",
+      price: 399.99,
+      image: "./images/produtos/jogo-halo-infinite-xbox.jpg",
+      id_categorys: 3,
+      name_categorys: "Xbox",
+      selected: false
+    },
+    {
+      id_product: 10,
+      category: "Switch",
+      fk_idcategorys: 4,
+      name_product: "Mario Kart 8",
+      price: 350.99,
+      image: "./images/produtos/mario-kart-8.jpg",
+      id_categorys: 4,
+      name_categorys: "Nintendo Switch",
+      selected: false
+    },
+    {
+      id_product: 11,
+      category: "Xbox",
+      fk_idcategorys: 3,
+      name_product: "Mortal Kombat X",
+      price: 299.99,
+      image: "./images/produtos/mortalKombat.jpg",
+      id_categorys: 3,
+      name_categorys: "Xbox",
+      selected: false
+    },
+    {
+      id_product: 6,
+      category: "Switch",
+      fk_idcategorys: 4,
+      name_product: "Pokemon Legends Arceus",
+      price: 200,
+      image: "./images/produtos/pokemon-legends-arceus.jpg",
+      id_categorys: 4,
+      name_categorys: "Nintendo Switch",
+      selected: false
+    },
+    {
+      id_product: 7,
+      category: "Ps4",
+      fk_idcategorys: 2,
+      name_product: "Red Dead Redemption",
+      price: 450,
+      image: "./images/produtos/Red-Dead-RedemptionII.jpg",
+      id_categorys: 2,
+      name_categorys: "Ps4",
+      selected: false
+    },
+    {
+      id_product: 3,
+      category: "Xbox",
+      fk_idcategorys: 3,
+      name_product: "Resident Evil Viii",
+      price: 350,
+      image: "./images/produtos/residentEvil.jpg",
+      id_categorys: 3,
+      name_categorys: "Xbox",
+      selected: false
+    },
+    {
+      id_product: 5,
+      category: "Ps5",
+      fk_idcategorys: 1,
+      name_product: "Spider Man Mile Morales",
+      price: 399,
+      image: "./images/produtos/spiderman-milemorales.jpg",
+      id_categorys: 1,
+      name_categorys: "Ps5",
+      selected: false
+    },
+    {
+      id_product: 8,
+      category: "Ps4",
+      fk_idcategorys: 2,
+      name_product: "Spider Man",
+      price: 180,
+      image: "./images/produtos/spiderman-ps4.jpg",
+      id_categorys: 2,
+      name_categorys: "Ps4",
+      selected: false
+    },
+    {
+      id_product: 12,
+      category: "Switch",
+      fk_idcategorys: 4,
+      name_product: "The legend of Zelda Breath of Wild",
+      price: 189.99,
+      image: "./images/produtos/zelda.jpg",
+      id_categorys: 4,
+      name_categorys: "Nintendo Switch",
+      selected: false
+    },
+];
+
+export default productsList;
